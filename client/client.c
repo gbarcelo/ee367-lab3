e6932be65f7c1e3f91146216a8ce40a848c5c93c
millarda <akm.scout@gmail.com>
2018-02-06 11:01:03 -1000

    testing stages

--- client/client.c
+++ client/client.c
@@ -106,7 +106,7 @@ int main(int argc, char *argv[])
 
 			case 'c': {		//
 				//scanf("%s", arg);
-				char arg = getchar();
+				char arg = fgetc(stdin);
 				char *f;
 				//printf("%s\n", );
 				printf("1\n");

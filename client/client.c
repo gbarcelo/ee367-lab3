7ea8aafa4bc9b144561cf46ea97d6576e77a402b
millarda <akm.scout@gmail.com>
2018-02-07 21:35:25 -1000

    manage f

--- client/client.c
+++ client/client.c
@@ -107,7 +107,7 @@ int main(int argc, char *argv[])
 			case 'c': {		//
 				testflag = 1;
 				scanf("%s", arg);
-				char f[128];
+				char *f;
 				//printf("%s\n", );
 				//memset(f, '\0',sizeof(f));
 				char buffera[128];
